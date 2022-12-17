a764a76776b0037415d3d09cc282aac6a84cab39
Nilesh Mali <nilesh@troplabs.com>
2022-12-17 20:32:43 +0530

    core: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script>
+	import 'carbon-components-svelte/css/white.css';
+	import {
+		Header,
+		HeaderNav,
+		HeaderNavItem,
+		HeaderNavMenu,
+		SkipToContent,
+		Content,
+		Grid,
+		Row,
+		Column
+	} from 'carbon-components-svelte';
+
+	let isSideNavOpen = false;
+</script>
+
+<Header company="Troplabs" platformName="Trongo" bind:isSideNavOpen>
+	<svelte:fragment slot="skip-to-content">
+		<SkipToContent />
+	</svelte:fragment>
+	<HeaderNav>
+		<HeaderNavItem href="/" text="Link 1" />
+		<HeaderNavItem href="/" text="Link 2" />
+		<HeaderNavItem href="/" text="Link 3" />
+		<HeaderNavMenu text="Menu">
+			<HeaderNavItem href="/" text="Link 1" />
+			<HeaderNavItem href="/" text="Link 2" />
+			<HeaderNavItem href="/" text="Link 3" />
+		</HeaderNavMenu>
+	</HeaderNav>
+</Header>
+
+<Content>
+	<slot />
+</Content>
