e4ed7e38ebd04e50b0a874ea80ab6a60a53ff281
Nilesh Mali <nilesh@troplabs.com>
2023-11-27 12:06:35 +0530

    core: initial setup

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    'nuxt-primevue',
+  ],
+  devtools: { enabled: true },
+})
